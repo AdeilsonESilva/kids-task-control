13ed955f2d297cb1c5345f1aa09920b7414ec641
Adeilson Silva <tbh.adeilson.silva@sympla.com.br>
2025-06-03 13:34:50 -0300

    fix layout menu

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Settings, Users, ListTodo, LogOut, BarChart2, DollarSign } from "lucide-react";
+import {
+  Settings,
+  Users,
+  ListTodo,
+  LogOut,
+  BarChart2,
+  DollarSign,
+} from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import {
@@ -9,6 +16,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
 } from "./ui/dropdown-menu";
 import { useState, useCallback } from "react";
 import { ChildrenManagementDialog } from "./children-management-dialog";
@@ -32,24 +42,6 @@ export function MainNav() {
 
   return (
     <div className="flex items-center gap-4">
-      {/* New Reports Menu */}
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <BarChart2 className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem asChild>
-            <Link href="/reports/payment-due">
-              <DollarSign className="mr-2 h-4 w-4" />
-              Valor a pagar
-            </Link>
-          </DropdownMenuItem>
-          {/* Future report items can be added here */}
-        </DropdownMenuContent>
-      </DropdownMenu>
-      
       <ThemeToggle />
 
       {!isChildrenDialogOpen && !isTaskDialogOpen ? (
@@ -69,6 +61,25 @@ export function MainNav() {
               <ListTodo className="mr-2 h-4 w-4" />
               Gerenciar Tarefas
             </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <BarChart2 className="mr-2 h-4 w-4" />
+                Relatórios
+              </DropdownMenuSubTrigger>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem asChild>
+                  <Link
+                    href="/reports/payment-due"
+                    className="flex items-center"
+                  >
+                    <DollarSign className="mr-2 h-4 w-4" />
+                    Valor a pagar
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuSub>
+            <DropdownMenuSeparator />
             <DropdownMenuItem onClick={signOut}>
               <LogOut className="mr-2 h-4 w-4" />
               Sair
